e2b176da417566546858a146ee3db28dba6dc9d6
Zoey <zoey@panties.moe>
2021-06-01 23:27:15 -0400

    Remove prefs

--- discordextrasprefs/discordExtrasRootListController.h
+++
@@ -1,7 +0,0 @@
-#import <Preferences/PSListController.h>
-#import <Preferences/PSSpecifier.h>
-#import <CepheiPrefs/HBRootListController.h>
-#import <CepheiPrefs/HBAppearanceSettings.h>
-
-@interface discordExtrasRootListController : HBRootListController
-@end
